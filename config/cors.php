45f5f92e5c260524d78842cb00462bff06206f2a
imranbanglaweb <md.imran1200@gmail.com>
2026-05-20 14:08:34 +0600

    Initial frontend commit

--- config/cors.php
+++ config/cors.php
@@ -25,7 +25,7 @@ return [
         'http://localhost:56374',
         'http://127.0.0.1:3000', 
         'http://127.0.0.1:3001',
-        'https://garibondhu360.nextdigihome.com',
+        'https://nextdigihome.com',
     ],
 
     'allowed_origins_patterns' => [
